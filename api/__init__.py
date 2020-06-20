d84b908b9206eb2c30893236321f43e3a7b44d94
asherpasha <asher.pasha@utoronto.ca>
2020-06-20 00:49:05 -0400

    Working on API Docs

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+from flasgger import Swagger
+
+swaggger_template = {
+  "swagger": "2.0",
+  "info": {
+    "title": "BAR API",
+    "description": "API for the Bio-Analytic Resource",
+    "version": "0.0.1"
+  },
+  "schemes": [
+    "http"
+  ]
+}
 
 # Configuration
 app = Flask(__name__)
+swagger = Swagger(app, template=swaggger_template)
 app.config['SQLALCHEMY_BINDS'] = {'annotations_lookup': 'mysql://USER:PASSWORD@localhost/annotations_lookup',
                                   'single_cell': 'mysql://USER:PASSWORD@localhost/single_cell'}
 
@@ -14,7 +28,7 @@ from api.resources.gene_alias import GeneAlias
 from api.resources.rnaseq_gene_expression import RNASeqGeneExpression
 
 # Gene Information
-api.add_resource(GeneAlias, '/gene_alias/<string:species>/<string:gene_id>', '/gene_alias')
+api.add_resource(GeneAlias, '/gene_alias/<string:species>/<string:gene_id>')
 
 # Gene Expression
 api.add_resource(RNASeqGeneExpression,
