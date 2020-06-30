aca59bea6576ab17c1e985716258269cfc038757
asherpasha <asher.pasha@utoronto.ca>
2020-06-30 12:29:56 -0400

    Switching from Flask-Restful to Flask-Restx

--- api/__init__.py
+++ api/__init__.py
@@ -1,72 +1,65 @@
 from os import environ
 from os.path import expanduser
 import redis
-from flasgger import Swagger
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Api
 
 
-def start_redis():
-    """
-    This functions starts connection to the Redis Server
-    :return: redis.Redis() instance
-    """
-    redis_password = ''
-    if environ.get('BAR'):
-        redis_password = environ.get('BAR_REDIS_PASSWORD')
+class BARRedis(redis.Redis):
+    def __init__(self):
+        """
+        This functions starts connection to the Redis Server
+        :return: redis.Redis() instance
+        """
+        # Set the Redis password if we are on the BAR
+        self.redis_password = ''
+        if environ.get('BAR'):
+            self.redis_password = environ.get('BAR_REDIS_PASSWORD')
+        super().__init__(password=self.redis_password)
 
-    bar_redis = redis.Redis(password=redis_password)
-    return bar_redis
 
+class BARApi:
+    def __init__(self):
+        """
+        Initialize BAR API class
+        """
+        self.bar_app = Flask(__name__)
 
-def create_app():
-    """
-    This function create the app
-    :return: Flask app
-    """
-    # Set up variables
-    swaggger_template = {
-        "swagger": "2.0",
-        "info": {
-            "title": "BAR API",
-            "description": "API for the Bio-Analytic Resource",
-            "version": "0.0.1"
-        },
-        "schemes": [
-            "http",
-            "https"
-        ]
-    }
+        # Load configuration
+        if environ.get('TRAVIS'):
+            # Travis
+            self.bar_app.config.from_pyfile(environ.get('TRAVIS_BUILD_DIR') + '/config/BAR_API.cfg', silent=True)
+        elif environ.get('BAR'):
+            # The BAR
+            self.bar_app.config.from_pyfile(environ.get('BAR_API_PATH'), silent=True)
+        else:
+            # Change this line if you want to load your own configuration
+            self.bar_app.config.from_pyfile(expanduser('~') + '/Asher/BAR_API.cfg', silent=True)
 
-    # Set host name on BAR
-    if environ.get('BAR'):
-        swaggger_template["host"] = "bar.utoronto.ca"
-        swaggger_template["basePath"] = "/api"
+        # Initialize the database
+        db.init_app(self.bar_app)
+        # Now add routes
+        api = Api(
+            title='BAR API',
+            version='0.0.1',
+            description='API for the Bio-Analytic Resource'
+        )
 
-    bar_app = Flask(__name__)
+        from api.resources.gene_information import gene_information
+        from api.resources.rnaseq_gene_expression import rnaseq_gene_expression
 
-    # Load configuration
-    if environ.get('TRAVIS'):
-        # Travis
-        bar_app.config.from_pyfile(environ.get('TRAVIS_BUILD_DIR') + '/config/BAR_API.cfg', silent=True)
-    elif environ.get('BAR'):
-        # The BAR
-        bar_app.config.from_pyfile(environ.get('BAR_API_PATH'), silent=True)
-    else:
-        # Change this line if you want to load your own configuration
-        bar_app.config.from_pyfile(expanduser('~') + '/Asher/BAR_API.cfg', silent=True)
+        api.add_namespace(gene_information)
+        api.add_namespace(rnaseq_gene_expression)
+        api.init_app(self.bar_app)
 
-    # Initialize the database
-    db.init_app(bar_app)
-    # Now add routes
-
-    from api.routes import add_routes
-    add_routes(bar_app)
-
-    # Initialize Swagger UI
-    Swagger(bar_app, template=swaggger_template)
-
-    return bar_app
+    def create_app(self):
+        """
+        This function create the app
+        :return: Flask app
+        """
+        # Set up variables
+        return self.bar_app
 
 
 ############################################################################################################################
@@ -75,7 +68,7 @@ def create_app():
 db = SQLAlchemy()
 
 # Initialize Redis
-r = start_redis()
+r = BARRedis()
 
 # Now create the app
-app = create_app()
+app = BARApi().create_app()
