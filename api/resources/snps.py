fb8bbddf46632c9fee2a93e4d3bd3688b060ee0b
Vin <VinLau@users.noreply.github.com>
2020-11-18 21:13:18 -0500

    made asher's stylistic changes

--- api/resources/snps.py
+++ api/resources/snps.py
@@ -1,7 +1,7 @@
 from flask_restx import Namespace, Resource
 from markupsafe import escape
 from sqlalchemy.exc import OperationalError
-from api.models.retrieve_snp import ProteinAlias, SnpsProteinJoin, SnpsTbl
+from api.models.poplar_nssnp import ProteinAlias, SnpsProteinJoin, SnpsTbl
 from api.utils.bar_utils import BARUtils
 from api import cache, db
 import re
@@ -13,55 +13,55 @@ snps = Namespace('SNPs', description='Information about SNPs', path='/snps')
 class ProteinName(Resource):
     def get(self):
         """ Define later TODO """
-        id = ['asdasddsa']
-        return BARUtils.success_exit(id)
+        my_id = ['asdasddsa']
+        return BARUtils.success_exit(my_id)
 
 
 @snps.route('/gene_alias/<string:gene_id>')
 class GeneNameAlias(Resource):
-    @snps.param('gene_id', _in='path', default='1')
+    @snps.param('gene_id', _in='path', default='Potri.001G000100.1')
     @cache.cached()
     def get(self, gene_id=''):
         """ TODO define endpoint """
-        resultsJSON = []
+        results_json = []
 
         # Escape input
         gene_id = escape(gene_id)
 
         try:
-            rows = db.session.query(ProteinAlias, SnpsProteinJoin, SnpsTbl).\
-                select_from(ProteinAlias).\
-                join(SnpsProteinJoin).\
-                join(SnpsTbl).\
+            rows = db.session.query(ProteinAlias, SnpsProteinJoin, SnpsTbl). \
+                select_from(ProteinAlias). \
+                join(SnpsProteinJoin). \
+                join(SnpsTbl). \
                 filter(ProteinAlias.gene_identifier == gene_id).all()
             print(rows)
             for protein, snpsjoin, snpstbl in rows:
-                itmLst = [
+                itm_lst = [
                     snpstbl.chromosome,
                     snpstbl.chromosomal_loci,
                     snpstbl.sample_id,
-                    "missense_variant",
-                    "MODERATE",
-                    "MISSENSE",
+                    'missense_variant',
+                    'MODERATE',
+                    'MISSENSE',
                     None,
-                    "p." + snpsjoin.ref_aa + str(snpsjoin.aa_pos) + snpsjoin.alt_aa
-                    + "/c." + str(snpsjoin.transcript_pos) + snpsjoin.ref_DNA + ">" + snpsjoin.alt_DNA,
+                    'p.' + snpsjoin.ref_aa + str(snpsjoin.aa_pos) + snpsjoin.alt_aa + '/c.' + str(snpsjoin.transcript_pos) +
+                    snpsjoin.ref_DNA + '>' + snpsjoin.alt_DNA,
                     None,
-                    re.sub('.\d$', "", protein.gene_identifier),
+                    re.sub(r'.\d$', '', protein.gene_identifier),
                     None,
                     None,
                     protein.gene_identifier
                 ]
-                resultsJSON.append(itmLst)
+                results_json.append(itm_lst)
                 print(protein.gene_identifier, snpsjoin.alt_aa, snpstbl.sample_id)
         except OperationalError as e:
             print(e)
             return BARUtils.error_exit('An internal error has occurred'), 500
-        # [resultsJSON.append(row.gene_identifier) for row in rows]
+        # [results_json.append(row.gene_identifier) for row in rows]
 
         # Return results if there are data
-        if len(resultsJSON) > 0:
+        if len(results_json) > 0:
             print('yay!')
-            return BARUtils.success_exit(resultsJSON)
+            return BARUtils.success_exit(results_json)
         else:
             return BARUtils.error_exit('There are no data found for the given gene')
