2aef7b3b7fb906fc11603d342c888f106dd4a956
asherpasha <asher.pasha@utoronto.ca>
2021-02-13 23:20:26 -0500

    Testing flask-sqlacodegen for generating database models

--- api/models/eplant2.py
+++ api/models/eplant2.py
@@ -1,8 +1,12 @@
 from api import db
 
 
-class isoforms(db.Model):
+class Isoforms(db.Model):
     __bind_key__ = 'eplant2'
     __tablename__ = 'isoforms'
+    __table_args__ = (
+        db.Index('idx_gene_isoform', 'gene', 'isoform'),
+    )
+
     gene = db.Column(db.String(10), primary_key=True)
     isoform = db.Column(db.String(12), primary_key=True)
