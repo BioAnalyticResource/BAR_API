4a9f4b3775d6d19f1d9fb56ef03fa383d33d8928
Vin <VinLau@users.noreply.github.com>
2021-04-09 16:09:22 -0400

    added line data/API and isoform extension for tomato

---
+++ config/databases/eplant_tomato.sql
@@ -0,0 +1,59 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Linux (x86_64)
+--
+-- Host: localhost    Database: eplant_tomato
+-- ------------------------------------------------------
+-- Server version	8.0.23-3
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `eplant_tomato`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `eplant_tomato` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `eplant_tomato`;
+
+--
+-- Table structure for table `isoforms`
+--
+
+DROP TABLE IF EXISTS `isoforms`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `isoforms` (
+  `gene` varchar(20) NOT NULL,
+  `isoform` varchar(24) NOT NULL,
+  KEY `idx_gene_isoform` (`gene`,`isoform`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `isoforms`
+--
+
+LOCK TABLES `isoforms` WRITE;
+/*!40000 ALTER TABLE `isoforms` DISABLE KEYS */;
+INSERT INTO `isoforms` VALUES ('Solyc00g005000','Solyc00g005000.3.1'),('Solyc00g005040','Solyc00g005040.3.1'),('Solyc00g18885','Solyc00g188850.3.1');
+/*!40000 ALTER TABLE `isoforms` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump copied and modified from eplant_poplar.sql, 2021-04-09
