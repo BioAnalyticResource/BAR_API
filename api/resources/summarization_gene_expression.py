78aa299eed5679563c0a532ee483d50e84370e8e
Bruno <brunocosper@gmail.com>
2021-04-20 19:50:42 -0400

    Fix input type for Cromwell call

--- api/resources/summarization_gene_expression.py
+++ api/resources/summarization_gene_expression.py
@@ -129,7 +129,7 @@ class SummarizationGeneExpressionSummarize(Resource):
                     + gtf
                     + """,
                         "geneSummarization.aliases": """
-                    + json["aliases"]
+                    + str(json["aliases"])
                     + """,
                         "geneSummarization.id": """
                     + key
