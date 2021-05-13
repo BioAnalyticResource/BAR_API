cc7c3f13d79f052ac164a2e49fef36f61d6f16a1
Vin <VinLau@users.noreply.github.com>
2021-05-13 14:08:18 -0400

    made small changes tomato sequences and tests, to follow ePlant JSON design, also small fix to tomato regex check

--- api/utils/bar_utils.py
+++ api/utils/bar_utils.py
@@ -49,9 +49,9 @@ class BARUtils:
         :param isoform_id: True if you want to verifiy isoform ID
         :return: True if valid
         """
-        if isoform_id and re.search(r"^Solyc0\dg\d{6}\.\d\.\d$", gene, re.I):
+        if isoform_id and re.search(r"^Solyc\d\dg\d{6}\.\d\.\d$", gene, re.I):
             return True
-        elif isoform_id is False and re.search(r"^Solyc0\dg\d{6}$", gene, re.I):
+        elif isoform_id is False and re.search(r"^Solyc\d\dg\d{6}$", gene, re.I):
             return True
         else:
             return False
