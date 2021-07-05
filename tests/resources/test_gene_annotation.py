f8f186906ede13b8aad39d5dcaa52d694760ecaa
lyy321 <50380391+lyy321@users.noreply.github.com>
2021-07-04 22:25:36 -0400

    linting

--- tests/resources/test_gene_annotation.py
+++ tests/resources/test_gene_annotation.py
@@ -5,10 +5,10 @@ from unittest import TestCase
 class TestIntegrations(TestCase):
     def setUp(self):
         self.app_client = app.test_client()
-    
+
     def test_keyword_search(self):
-        test_query="alpha-1 protein"
+        test_query = "alpha-1 protein"
         response = self.app_client.get(f"/gene_annotation/{test_query}").json
-        self.assertEqual(response['status'],'success')
-        annotation=response['result'][0]['gene_annotation']
-        self.assertIn(test_query,annotation)
+        self.assertEqual(response["status"], "success")
+        annotation = response["result"][0]["gene_annotation"]
+        self.assertIn(test_query, annotation)
