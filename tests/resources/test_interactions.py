4af9ac81bda144e2322b2bf4a9bc76ebe7b76434
asherpasha <asher.pasha@utoronto.ca>
2021-12-06 00:52:15 -0500

    Working on eFP Images.

--- tests/resources/test_interactions.py
+++ tests/resources/test_interactions.py
@@ -17,43 +17,36 @@ class TestIntegrations(TestCase):
         # Valid request rice
         response = self.app_client.get("/interactions/rice/LOC_Os01g52560")
         expected = {
-          "wasSuccessful": True,
-          "data" :
-          [
-              {
-                "protein_1": "LOC_Os01g01080",
-                "protein_2": "LOC_Os01g52560",
-                "total_hits": 1,
-                "Num_species": 1,
-                "Quality": 1,
-                "pcc": 0.65
-              },
-              {
-                "protein_1": "LOC_Os01g52560",
-                "protein_2": "LOC_Os01g73310",
-                "total_hits": 1,
-                "Num_species": 1,
-                "Quality": 1,
-                "pcc": -0.116
-              }
-          ]
+            "wasSuccessful": True,
+            "data": [
+                {
+                    "protein_1": "LOC_Os01g01080",
+                    "protein_2": "LOC_Os01g52560",
+                    "total_hits": 1,
+                    "Num_species": 1,
+                    "Quality": 1,
+                    "pcc": 0.65,
+                },
+                {
+                    "protein_1": "LOC_Os01g52560",
+                    "protein_2": "LOC_Os01g73310",
+                    "total_hits": 1,
+                    "Num_species": 1,
+                    "Quality": 1,
+                    "pcc": -0.116,
+                },
+            ],
         }
         self.assertEqual(response.json, expected)
 
         # Invalid species
         response = self.app_client.get("/interactions/poplar/abc")
-        expected = {
-          "wasSuccessful": False,
-          "error": "Invalid species or gene ID"
-        }
+        expected = {"wasSuccessful": False, "error": "Invalid species or gene ID"}
         self.assertEqual(response.json, expected)
 
         # Invalid gene id
         response = self.app_client.get("/interactions/rice/abc")
-        expected = {
-          "wasSuccessful": False,
-          "error": "Invalid species or gene ID"
-        }
+        expected = {"wasSuccessful": False, "error": "Invalid species or gene ID"}
         self.assertEqual(response.json, expected)
 
         # Gene does not exist
