7544267cda6a629a77ce88119161655c2cede327
Vin <VinLau@users.noreply.github.com>
2022-02-16 01:54:28 -0500

    added gene rice annotation db, along with POST request for gene annotations for rice (for now - can be expanded later)

--- api/resources/gene_annotation.py
+++ api/resources/gene_annotation.py
@@ -1,10 +1,13 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
+from flask import request
 from markupsafe import escape
 from sqlalchemy.exc import OperationalError
 from api.models.eplant_poplar import GeneAnnotation as eplant_poplar_annotation
+from api.models.eplant_rice import GeneAnnotation as eplant_rice_annotation
 from api.models.eplant_tomato import GeneAnnotation as eplant_tomato_annotation
 from api.models.eplant2 import AgiAnnotation, TAIR10, GeneRIFs
 from api.utils.bar_utils import BARUtils
+from marshmallow import Schema, ValidationError, fields as marshmallow_fields
 
 
 gene_annotation = Namespace(
@@ -22,6 +25,7 @@ class GeneAnnotation(Resource):
         annotation_db_list = {
             "tomato": eplant_tomato_annotation,
             "poplar": eplant_poplar_annotation,
+            "rice"  : eplant_rice_annotation,
             "arabidopsis": [AgiAnnotation, TAIR10, GeneRIFs],
         }
 
@@ -94,3 +98,73 @@ class GeneAnnotation(Resource):
         else:
             # return first 10 matches
             return {"status": "success", "query": query, "result": res[:10]}
+
+
+anntn_post_ex = gene_annotation.model(
+    "AntnRiceGenes",
+    {
+        "species": fields.String(required=True, example="rice"),
+        "genes": fields.List(
+            required=True,
+            example=["LOC_Os01g01010", "LOC_Os01g01050"],
+            cls_or_instance=fields.String,
+        ),
+    },
+)
+
+
+class GeneIntrnsSchema(Schema):
+    species = marshmallow_fields.String(required=True)
+    genes = marshmallow_fields.List(cls_or_instance=marshmallow_fields.String)
+
+
+@gene_annotation.route("/")
+class GeneAnnotationPost(Resource):
+    @gene_annotation.expect(anntn_post_ex)
+    def post(self, query=""):
+        """
+        Returns gene annotation(s) give a set of gene(s) for a species
+        Supported species: 'rice'
+        """
+
+        json_data = request.get_json()
+
+        try:
+            json_data = GeneIntrnsSchema().load(json_data)
+        except ValidationError as err:
+            return BARUtils.error_exit(err.messages), 400
+
+        species = json_data["species"].lower()
+        genes = json_data["genes"]
+
+        if species == "rice":
+            for gene in genes:
+                if not BARUtils.is_rice_gene_valid(gene):
+                    return BARUtils.error_exit("Invalid gene id"), 400
+
+            try:
+                rows = eplant_rice_annotation.query.filter(
+                        eplant_rice_annotation.gene.in_(genes)
+                ).all()
+                if len(rows) == 0:
+                    return (
+                        BARUtils.error_exit(
+                            "No data for the given species/genes"
+                        ),
+                        400,
+                    )
+                else:
+                    print(rows)
+                    res = [
+                        {
+                            "gene": i.gene,
+                            "annotation": i.annotation,
+                        }
+                        for i in rows
+                    ]
+                    return BARUtils.success_exit(res)
+            except OperationalError:
+                return BARUtils.error_exit("An internal error has occurred."), 500
+
+        else:
+            return BARUtils.error_exit("Invalid species"), 400
