b0c78b1c3ee7ff15e1720608c5af96694a2faf43
Bruno Pereira <brunocosper@gmail.com>
2022-03-17 19:04:00 -0400

    Change table creation

--- api/models/summarization.py
+++ api/models/summarization.py
@@ -4,6 +4,8 @@ from api import summarization_db as db
 class SummarizationGeneExpression(db.Model):
     __bind_key__ = "summarization"
     __tablename__ = "summarization"
+    proj_id = db.Column(db.String(5), nullable=False)
+    sample_id = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue())
     data_probeset_id = db.Column(db.String(24), primary_key=True)
     data_signal = db.Column(db.Integer, primary_key=True)
     data_bot_id = db.Column(db.String(32), primary_key=True)
