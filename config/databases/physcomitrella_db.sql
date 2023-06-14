8fac53c016f91e78a8df1519609ef08fbf18bde2
asherpasha <asher.pasha@utoronto.ca>
2023-06-13 23:30:56 -0400

    Added Brassica rappa and Physcomitrella

---
+++ config/databases/physcomitrella_db.sql
@@ -0,0 +1,64 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+--
+-- Host: localhost    Database: physcomitrella_db
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `physcomitrella_db`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `physcomitrella_db` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `physcomitrella_db`;
+
+--
+-- Table structure for table `sample_data`
+--
+
+DROP TABLE IF EXISTS `sample_data`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sample_data` (
+  `sample_id` varchar(30) NOT NULL DEFAULT '',
+  `proj_id` varchar(30) NOT NULL DEFAULT '',
+  `data_probeset_id` varchar(40) NOT NULL DEFAULT '',
+  `data_signal` float NOT NULL,
+  `data_bot_id` varchar(40) NOT NULL,
+  PRIMARY KEY (`sample_id`,`proj_id`,`data_probeset_id`),
+  KEY `data_probeset_id` (`data_probeset_id`,`data_bot_id`,`data_signal`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sample_data`
+--
+-- WHERE:  data_probeset_id='Pp1s10008_1V6.1' and sample_id=1
+
+LOCK TABLES `sample_data` WRITE;
+/*!40000 ALTER TABLE `sample_data` DISABLE KEYS */;
+INSERT INTO `sample_data` VALUES ('1','1','Pp1s10008_1V6.1',23.2,'Spores');
+/*!40000 ALTER TABLE `sample_data` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-13 22:49:45
