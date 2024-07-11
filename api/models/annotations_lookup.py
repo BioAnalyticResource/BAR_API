a23b04fbe0ece3bd3353a6e8d85414da0f616ccb
Vin <VinLau@users.noreply.github.com>
2024-07-11 13:56:31 -0400

    Added config files to make microarray expression endpoint work (config, init, annotations lookup model)

--- api/models/annotations_lookup.py
+++ api/models/annotations_lookup.py
@@ -10,3 +10,13 @@ class AgiAlias(db.Model):
     agi: db.Mapped[str] = db.mapped_column(db.String(30), primary_key=True, nullable=False)
     alias: db.Mapped[str] = db.mapped_column(db.String(30), primary_key=True, nullable=False)
     date: db.Mapped[datetime] = db.mapped_column(db.Date, primary_key=True, nullable=False)
+
+
+class AtAgiLookup(db.Model):
+    __bind_key__ = "annotations_lookup"
+    __tablename__ = "at_agi_lookup"
+    __table_args__ = (db.Index("probeset_date_agi", "probeset", "agi", "date"),)
+
+    probeset: db.Mapped[str] = db.mapped_column(db.String(60), primary_key=True, nullable=False)
+    agi: db.Mapped[str] = db.mapped_column(db.String(30), primary_key=True, nullable=False)
+    date: db.Mapped[datetime] = db.mapped_column(db.Date, primary_key=True, nullable=False)
